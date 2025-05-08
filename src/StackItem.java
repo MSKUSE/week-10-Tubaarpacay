4cca0f44c6e8372736d317ee600e93addc77ec92
Tubaarpacay <tubaarpacay@posta.mu.edu.tr>
2025-05-08 13:20:56 +0300

    Update StackItem.java

--- src/StackItem.java
+++ src/StackItem.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class StackItem {
+    private Object item;
+    private  StackItem next;
+
+    public StackItem(){
+
+    }
+    public StackItem(Object item) {
+        this.item = item;
+    }
+
+    public StackItem(Object item, StackItem next) {
+        this.item = item;
+        this.next = next;
+    }
+
+    public Object getItem() {
+        return item;
+    }
+
+    public void setItem(Object item) {
+        this.item = item;
+    }
+
+    public StackItem getNext() {
+        return next;
+    }
+
+    public void setNext(StackItem next) {
+        this.next = next;
+    }
 }
+
