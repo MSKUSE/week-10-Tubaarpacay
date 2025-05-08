6ba9b607d189367c8aca788ef1eb4c32bdbc1e70
Tubaarpacay <tubaarpacay@posta.mu.edu.tr>
2025-05-08 13:20:12 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Main {
-}
+    public static void main(String[] args) {
+        /*
+        Stack stack =new StackArray();
+        stack.push("String");
+        stack.push(12);
+        stack.peek();
+        System.out.println("Pop= " + stack.pop());
+        stack.peek();
+        System.out.println("Pop=" + stack.pop());
+        System.out.println("I empty " + stack.isEmpty());
+        stack.peek();
+        System.out.println("Pop=" + stack.pop());
+         */
+        StackLinkedList stack=new StackLinkedList();
+        stack.push(5);
+        stack.push("It is a string") ;
+        stack.peek();
+        System.out.println(stack.top.getItem());
+        System.out.println(stack.top.getNext().getItem());
+        System.out.println(stack.top.getNext().getNext()
